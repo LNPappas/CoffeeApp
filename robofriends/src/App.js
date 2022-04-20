dc6e93c2bc95ef7fd68666d29583c56527a5b3b9
LNPappas <LPappas@gmail.com>
2022-04-20 16:26:36 -0700

    api & scroll

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -2,15 +2,25 @@ import "./App.css";
 import React, { Component } from "react";
 import CardList from "./components/CardList";
 import SearchBox from "./components/SearchBox";
-import { robots } from "./components/robots";
+import Scroll from "./components/Scroll";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
 
   onSearchChange = (event) => {
@@ -23,15 +33,26 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="App tc">
-        <h1 id="title" className="f1">
-          RoboFriends
+
+    if (this.state.robots.length === 0) {
+      return (
+        <h1 className="tc" id="title">
+          Loading...
         </h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="App tc">
+          <h1 id="title" className="f1">
+            RoboFriends
+          </h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
