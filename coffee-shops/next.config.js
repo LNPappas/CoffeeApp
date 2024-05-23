4a560412405fd8e00e97eebbd75f89d15498bf65
LNPappas <LPappas@gmail.com>
2024-05-23 09:02:40 -0700

    npm update, next.config headers

--- coffee-shops/next.config.js
+++ coffee-shops/next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   basePath: "/CoffeeApp",
   output: "export",
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
