ba0716e6c6918ac6f745c1266f3ba0e7d979d34f
LNPappas <LPappas@gmail.com>
2024-05-23 09:06:51 -0700

    added debugger to action, update next.config

--- coffee-shops/next.config.js
+++ coffee-shops/next.config.js
@@ -3,19 +3,6 @@ const nextConfig = {
   basePath: "/CoffeeApp",
   output: "export",
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
